1ac6193bd6cc7b8936cb64f2545e8325fad97486
thomasbremon-hash <thomas.bremon@logicielpourtous.com>
2026-04-01 15:00:34 +0200

    feat: migration add email/codeAPE/iban/bic to entreprise table

---
+++ migrations/Version20260401130012.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260401130012 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE entreprise ADD email VARCHAR(255) DEFAULT NULL, ADD code_ape VARCHAR(10) DEFAULT NULL, ADD iban VARCHAR(34) DEFAULT NULL, ADD bic VARCHAR(11) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE entreprise DROP email, DROP code_ape, DROP iban, DROP bic');
+    }
+}
